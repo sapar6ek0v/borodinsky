82a3bf178d8ccdeca40760533b429863af2aa4b5
sapar6ek0v <razetka0309@gmail.com>
2021-10-19 18:05:38 +0300

    add adaptive

--- js/script.js
+++ js/script.js
@@ -4,3 +4,13 @@ const menu = document.getElementById('ul-header-menu')
 button.addEventListener('click', () =>{
     menu.classList.toggle('header-ul-mobile')
 })
+
+
+const links = document.querySelectorAll('.header-link')
+
+links.forEach( (elem) =>{
+        elem.addEventListener('click', () =>{
+            menu.classList.remove('header-ul-mobile')
+        })
+    }
+)
